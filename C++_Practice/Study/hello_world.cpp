66fc4503adb52fca4033bc742983ddd2611f938a
xianyou <2171943026@qq.com>
2026-07-27 23:37:40 +0800

    成功开启C++

---
+++ C++_Practice/Study/hello_world.cpp
@@ -0,0 +1,11 @@
+//
+// Created by hp on 2026/7/27.
+//
+#include "iostream"
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
---
+++ Java_Practice/Algorithm problem/src/problem_demo/Demo3.java
@@ -0,0 +1,35 @@
+package problem_demo;
+
+import java.util.Arrays;
+
+public class Demo3 {
+    public static void main(String[] args) {
+        int[] stones = {2,7,4,1,8,1};
+        System.out.println(lastStoneWeight(stones));
+    }
+
+    public static int lastStoneWeight(int[] stones) {
+        int n = stones.length;
+        if(n==1)
+            return stones[0];
+        while(n > 2){
+            Arrays.sort(stones);
+            int a = stones[n-1];
+            int b = stones[n-2];
+            if(a==b) {
+                n-=2;
+                continue;
+            }
+            stones[n-2] = a - b;
+            n--;
+        }
+        Arrays.sort(stones);
+        if(n==2) {
+            if (stones[0] == stones[1])
+                return 0;
+            else
+                return stones[1] - stones[0];
+        }
+        return stones[0];
+    }
+}
